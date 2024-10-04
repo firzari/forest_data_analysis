3b9aca0ef0ba327236408533fb3eef7c9a748a6f
firzari <firza.riany@climateanalytics.org>
2024-10-04 10:00:32 +0000

    Convert netCDF to GeoTiff

--- src/geo_tools/utils.py
+++ src/geo_tools/utils.py
@@ -5,11 +5,11 @@ import pandas as pd
 
 ### FUNCTIONS ###
 
-# Importing NetCDF file 
-def import_forest(file_id: str) -> xr.DataArray:
 
+# Importing NetCDF file
+def import_forest(file_id: str) -> xr.DataArray:
     """Importing forest dataset whose naming convention starts with "Forest4model_v1_".
-    Data is downloadable in https://gitlab.iiasa.ac.at/forestnavigator/wp2/public
+    Data can be downloaded from https://gitlab.iiasa.ac.at/forestnavigator/wp2/public
 
     Args:
         file_id (str): File name of the forest data that can be identified in the words following
@@ -30,9 +30,9 @@ def import_forest(file_id: str) -> xr.DataArray:
 
     return ds
 
+
 # Importing subregion borders
 def import_boundary() -> gpd.GeoDataFrame:
-
     """Importing the default shapefile containing global country borders
 
     Returns:
@@ -46,13 +46,10 @@ def import_boundary() -> gpd.GeoDataFrame:
 
     return subregion_borders
 
-# Clipping forest data to country boundaries
-def clip_array(
-        iso_code: str, 
-        xarray_id: str
-    ):
 
-    """Clipping xarray data of larger bounds to a smaller bounds, specifically this function
+# Clipping forest data to country boundaries
+def clip_array(iso_code: str, xarray_id: str):
+    """Clipping xarray data of larger bounds to smaller bounds, specifically this function
     clips xarray data of larger bounds/spatial extent to the spatial extent of a selected country
 
     Args:
@@ -70,71 +67,100 @@ def clip_array(
 
     # Import country border
     shp_border = import_boundary()
-
-    # Reprojection to the CRS in subregion borders
-    ds_reproject = xarray_input.rio.write_crs(shp_border.crs)
-
     border = shp_border.loc[[iso_code]]
-    country_forest = ds_reproject.rio.clip_box(
-        *border.total_bounds
-    ).rio.clip(border.geometry, crs=ds_reproject.rio.crs)
+
+    def to_clip(xarray_w_crs, border_gpd):
+        clip_xarray = xarray_w_crs.rio.clip_box(*border_gpd.total_bounds).rio.clip(
+            border_gpd.geometry, crs=xarray_w_crs.rio.crs
+        )
+        return clip_xarray
+
+    if xarray_input.rio.crs is None and border.crs is not None:
+        print(
+            "CRS information for the raster is missing. Clipping and reprojecting the raster to match the CRS of the boundary shapefile"
+        )
+
+        # Reprojection the raster file
+        ds_reproject = xarray_input.rio.write_crs(shp_border.crs)
+
+        # Clipping the raster
+        country_forest = to_clip(ds_reproject, border)
+
+    elif xarray_input is not None and border.crs is None:
+        print(
+            "CRS information for the boundary shapefile is missing. Reprojecting the boundary shapefile to match the CRS of the raster file"
+        )
+
+        # Reprojecting the boundary shapefile
+        border = border.set_crs(xarray_input.rio.crs, allow_override=True)
+
+        # Clipping the raster
+        country_forest = to_clip(xarray_input, border)
+
+    else:
+        print("Either the raster or the boundary shapefile must have a CRS information")
+
+    # Add iso information
+    country_forest = country_forest.assign_attrs(
+        iso=iso_code, crs=country_forest.rio.crs
+    )
+
+    print(f"CRS for the clipped raster is set to {country_forest.rio.crs}")
+    print(f"CRS for the boundary shapefile is set to {border.crs.to_epsg()}")
 
     xarray_input.close()
 
     return country_forest, border
 
+
 # Calculate area for each histogram bin
 def define_class(
-        iso_code: str, 
-        xarray_id: str,
-        forest_layer_year: int,
-        start_step: float,
-        end_step: float,
-        delta_diff: float,
-        max_val: float,
-        ) -> pd.DataFrame: 
-    
+    iso_code: str,
+    xarray_id: str,
+    forest_layer_year: int,
+    start_step: float,
+    end_step: float,
+    delta_diff: float,
+    max_val: float,
+) -> pd.DataFrame:
     """Dividing data values to specific and customisable bin classes. Can be used to generate a bar plot
     visualising the distribution of data values into bin classes and the frequency of each bin class
 
     Args:
-        iso_code (str): 
+        iso_code (str):
             The ISO3 code for the selected country i.e. "DEU"
-        xarray_id (str): 
+        xarray_id (str):
             File name of the forest data that can be identified in the words following
             the naming convention "Forest4model_v1_" i.e. "Canopy_height" is the words following
             "Forest4model_v1_Canopy_height.nc"
-        forest_layer_year (int): 
+        forest_layer_year (int):
             The data acquisition year as shown in the attribute table of xarray_id
-        start_step (float): 
+        start_step (float):
             The starting value to initiate the creation of the first bin class, which is usually the minimum value
-            of the dataset. 
+            of the dataset.
             The value range of each bin is specified by delta_diff (description below).
             The next bin classes will be created automatically using the initiated start_step.
-            For example, the minimum value is 1.0, then we can pass it into the start_step argument for 
+            For example, the minimum value is 1.0, then we can pass it into the start_step argument for
             creating the first bin class. Then we set the value range to be included in each bin class, for example
-            4.0, to be passed into delta_diff. Then it will create a first bin with the following range: 1.0 - 5.0 
+            4.0, to be passed into delta_diff. Then it will create a first bin with the following range: 1.0 - 5.0
             (delta_diff = 4.0)
-        end_step (float): 
+        end_step (float):
             The ending value for the the first bin class. If you'd like to have a delta_diff of 4.0, then the
-            end_value could be set to 5.0 # Need to be modified, I think we need only start and delta_diff # 
-        delta_diff (float): 
+            end_value could be set to 5.0 # Need to be modified, I think we need only start and delta_diff #
+        delta_diff (float):
             The value range for each bin class
-        max_val (float): 
+        max_val (float):
             Maximum value from the dataset.
 
     Returns:
-        pd.DataFrame: 
+        pd.DataFrame:
             A data frame containing the range of each bin class and its frequency, as well as ISO3 identification,
             total forest cover for comparison, and percentage area for each bin class over total forest cover
     """
 
     # Clipping forest cover fraction
-    country_forest, iso_border = clip_array(
-        iso_code,
-        xarray_id
-    )
-    
+    country_forest, iso_border = clip_array(iso_code, xarray_id)
+
     start = start_step
     end = end_step
     area = []
@@ -144,7 +170,7 @@ def define_class(
         country_forest_clean = country_forest.where(country_forest < 28)
         country_forest.close()
 
-    else: 
+    else:
         country_forest_clean = country_forest
         country_forest.close()
 
@@ -156,11 +182,8 @@ def define_class(
         )
 
         calc_area = (
-            forest_data
-            .count(["latitude", "longitude"]) 
-            .values
-            .flatten()[0]
-        ) / 1e+6 # in Mha
+            (forest_data.count(["latitude", "longitude"]).values.flatten()[0]) / 1e6
+        )  # in Mha
 
         # print(calc_area)
 
@@ -176,44 +199,28 @@ def define_class(
 
         start += delta_step
         end += delta_step
-    
-    # Convert to dataframe 
+
+    # Convert to dataframe
     forest_class_df = pd.DataFrame(area)
 
     # Rename column
-    forest_class_df.rename(columns={0 : "Value"}, inplace=True)
+    forest_class_df.rename(columns={0: "Value"}, inplace=True)
 
     # Reset index and rename it into variable
     forest_class_df = forest_class_df.reset_index()
-    forest_class_df.rename(columns={"index" : "Variable"}, inplace=True)
+    forest_class_df.rename(columns={"index": "Variable"}, inplace=True)
 
     # Add a column for iso code
-    forest_class_df.insert(
-        loc=0,
-        column="Region",
-        value=iso_code
-    )
+    forest_class_df.insert(loc=0, column="Region", value=iso_code)
 
     # Add a column for var_uid
     var_uid = xarray_id.lower()
-    forest_class_df.insert(
-        loc=2,
-        column="Var_uid",
-        value=var_uid
-    )
+    forest_class_df.insert(loc=2, column="Var_uid", value=var_uid)
 
     # Add a column for year
-    forest_class_df.insert(
-        loc=3,
-        column="Year",
-        value=forest_layer_year
-    )
+    forest_class_df.insert(loc=3, column="Year", value=forest_layer_year)
 
     # Add a column for unit
-    forest_class_df.insert(
-        loc=4,
-        column="Unit",
-        value="Million hectares"
-    )
+    forest_class_df.insert(loc=4, column="Unit", value="Million hectares")
 
     return forest_class_df
